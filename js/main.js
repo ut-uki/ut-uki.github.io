a5965103f7ac42ac98ffb3225282f562719533ba
“ut-uki” <80034832+ut-uki@users.noreply.github.com>
2023-07-17 13:35:36 +0900

    心理学対応

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ function make_accordion(data, num) {
   tag_button.setAttribute("data-bs-target", "#collapse"+num)
   tag_button.setAttribute("area-expanded", "false");
   tag_button.setAttribute("area-controls", "collapse"+num);
-  tag_button.textContent = num + " " + data[0];
+  tag_button.textContent = num + " " + data[1];
   tag_h2_.appendChild(tag_button)
 
   let tag_div2 = document.createElement('div');
@@ -53,9 +53,33 @@ function make_accordion(data, num) {
 
   let tag_div3 = document.createElement('div');
   tag_div3.className = "accordion-body";
-  tag_div3.textContent = data[3];
   tag_div2.appendChild(tag_div3);
 
+  let tag_strong = document.createElement('strong');
+  tag_strong.textContent = data[2]
+  tag_div3.appendChild(tag_strong)
+
+  let tag_p = document.createElement('p');
+  tag_p.textContent = "\n" + data[8];
+  tag_div3.appendChild(tag_p)
+
+  let tag_div4 = document.createElement('div');
+  tag_div4.className = "genre";
+  tag_div4.textContent = "ジャンル:";
+  tag_div3.appendChild(tag_div4);
+  let tag_a1 = document.createElement('a');
+  tag_a1.textContent = data[3]
+  tag_a1.href = "#"
+  tag_div4.appendChild(tag_a1)
+
+  let tag_div5 = document.createElement('div');
+  tag_div5.className = "genre";
+  tag_div5.textContent = "関連:";
+  tag_div3.appendChild(tag_div5);
+  let tag_a2 = document.createElement('a');
+  tag_a2.textContent = data[4]
+  tag_a2.href = "#"
+  tag_div5.appendChild(tag_a2)
 
 }
 
@@ -78,21 +102,22 @@ function make_list(n) {
   clone = accordion.cloneNode(false);
   accordion.parentNode.replaceChild(clone, accordion);
 
-  results = read_csv("js/uriage.csv").slice(1,n+2)
+  results = read_csv("js/心理学単語帳v2.csv").slice(1,n+1)
+  console.log(results)
 
   if (document.getElementById("flexSwitchCheckChecked").checked){
-    results_ = arrayShuffle(results)
-  }else{
-    results_ = results
+    results_ = arrayShuffle(results.slice())
+  } else{
+    results_ = results.slice().sort()
   }
 
-  for (let i = 1; i < n+1; i++) {
-    make_accordion(results_[i], i)
-    console.log(i)
+  for (let i = 0; i < n; i++) {
+    make_accordion(results_[i], i+1)
   }
 }
 
-result = read_csv("js/uriage.csv").slice(1,20)
+console.log(arrayShuffle([1,2,3,4,5]))
+// result = read_csv("js/心理学単語帳v1.csv").slice(1,10)
 
 // var form = document.forms.myform;
 //
@@ -101,6 +126,6 @@ result = read_csv("js/uriage.csv").slice(1,20)
 // for (let i = 1; i < 20; i++) {
 //     make_accordion(result[i], i)
 // }
-make_list(20)
+make_list(10)
 
 
